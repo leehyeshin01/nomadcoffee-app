cacf95d2d78f0196b4396b0d4f01ced93661b83a
leehyeshin01 <72959271+leehyeshin01@users.noreply.github.com>
2024-03-14 05:24:31 +0900

    Coffee Shop Component

--- apollo.js
+++ apollo.js
@@ -4,30 +4,26 @@ import {
     InMemoryCache,
     makeVar,
   } from "@apollo/client";
+  import { onError } from "@apollo/client/link/error";
   import { setContext } from "@apollo/client/link/context";
+  import { offsetLimitPagination } from "@apollo/client/utilities";
   import AsyncStorage from "@react-native-async-storage/async-storage";
-  
   export const isLoggedInVar = makeVar(false);
   export const tokenVar = makeVar("");
-  
   const TOKEN = "token";
-  
   export const logUserIn = async (token) => {
     await AsyncStorage.setItem(TOKEN, token);
     isLoggedInVar(true);
     tokenVar(token);
   };
-  
   export const logUserOut = async () => {
     await AsyncStorage.removeItem(TOKEN);
     isLoggedInVar(false);
     tokenVar(null);
   };
-  
   const httpLink = createHttpLink({
     uri: "http://localhost:4000/graphql",
   });
-  
   const authLink = setContext((_, { headers }) => {
     return {
       headers: {
@@ -37,8 +33,27 @@ import {
     };
   });
   
+  const onErrorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+      console.log(`GraphQL Error`, graphQLErrors);
+    }
+    if (networkError) {
+      console.log("Network Error", networkError);
+    }
+  });
+  
+  export const cache = new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          seeFeed: offsetLimitPagination(),
+        },
+      },
+    },
+  });
+  
   const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
+    link: authLink.concat(onErrorLink).concat(httpLink),
+    cache,
   });
   export default client;
--- fragments.js
+++ fragments.js
@@ -1,5 +1,4 @@
 import { gql } from "@apollo/client";
-
 export const PHOTO_FRAGMENT = gql`
   fragment PhotoFragment on Photo {
     id
@@ -9,7 +8,6 @@ export const PHOTO_FRAGMENT = gql`
     isLiked
   }
 `;
-
 export const COMMENT_FRAGMENT = gql`
   fragment CommentFragment on Comment {
     id
@@ -21,4 +19,28 @@ export const COMMENT_FRAGMENT = gql`
     isMine
     createdAt
   }
+`;
+export const USER_FRAGMENT = gql`
+  fragment UserFragment on User {
+    id
+    username
+    avatar
+    isFollowing
+    isMe
+  }
+`;
+
+export const FEED_PHOTO = gql`
+  fragment FeedPhoto on Photo {
+    ...PhotoFragment
+    user {
+      id
+      username
+      avatar
+    }
+    caption
+    createdAt
+    isMine
+  }
+  ${PHOTO_FRAGMENT}
 `;
